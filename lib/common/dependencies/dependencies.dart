a2275efd7e8e5911e6def618369371dcf19a88d6
Jady McIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2025-07-10 19:54:55 +0200

    initial auth

---
+++ lib/common/dependencies/dependencies.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_it/get_it.dart';
+import 'package:godsufficient/features/auth/data/datasources/fb_auth_datasource.dart';
+import 'package:godsufficient/features/auth/data/repositories/auth_repo.dart';
+import 'package:godsufficient/features/auth/data/repositories/auth_repo_impl.dart';
+import 'package:godsufficient/features/auth/domain/usecases/sign_in.dart';
+import 'package:godsufficient/features/auth/presentation/cubit/auth_cubit.dart';
+
+final sl = GetIt.instance;
+
+void initAuth() {
+  // Data
+  sl.registerLazySingleton(() => FirebaseAuthDatasource(FirebaseAuth.instance));
+  sl.registerLazySingleton<AuthRepo>(() => AuthRepositoryImpl(sl()));
+
+  // Use-cases
+  sl.registerLazySingleton(() => SignIn(sl()));
+  // sl.registerLazySingleton(() => Register(sl()));
+  // sl.registerLazySingleton(() => SignOut(sl()));
+  // sl.registerLazySingleton(() => WatchAuth(sl()));
+
+  // Cubit
+  sl.registerFactory(() => AuthCubit(signIn: sl(), register: sl(), signOut: sl(), watchAuth: sl()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:godsufficient/common/dependencies/dependencies.dart';
 import 'package:godsufficient/common/router/app_router.dart';
+import 'package:godsufficient/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:godsufficient/firebase_options.dart';
 import 'package:godsufficient/theme/theme.dart';
 
@@ -14,12 +17,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: appRouter,
-      title: 'God Sufficient',
-      theme: lightTheme(),
-      darkTheme: darkTheme(),
-      themeMode: ThemeMode.system,
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => sl<AuthCubit>())],
+      child: MaterialApp.router(
+        routerConfig: appRouter,
+        title: 'God Sufficient',
+        theme: lightTheme(),
+        darkTheme: darkTheme(),
+        themeMode: ThemeMode.system,
+      ),
     );
   }
 }
