15da5b11a92971e6c4c79fe3908ad9e7c0f5a41e
Jady McIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2025-07-11 15:42:49 +0200

    fixed sign up and ran on android

--- lib/features/auth/presentation/sign_in.dart
+++ lib/features/auth/presentation/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:godsufficient/common/widgets/page.dart';
 import 'package:godsufficient/features/auth/presentation/cubit/auth_cubit.dart';
 
 class SignInPage extends StatelessWidget {
@@ -11,33 +12,38 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocConsumer<AuthCubit, AuthState>(
-        listener: (context, state) {
-          if (state is Authenticated) context.go('/home');
-          if (state is AuthFailure) ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(state.message)));
-        },
-        builder: (context, state) {
-          if (state is AuthLoading) return const Center(child: CircularProgressIndicator());
-          return Padding(
-            padding: const EdgeInsets.all(24),
-            child: Column(
-              children: [
-                TextField(controller: _email),
-                TextField(controller: _pw, obscureText: true),
-                ElevatedButton(
-                  onPressed: () => context.read<AuthCubit>().login(_email.text.trim(), _pw.text.trim()),
-                  child: const Text('Sign In'),
-                ),
-                TextButton(
-                  onPressed: () => context.go('/sign-up'),
-                  child: const Text('Create account'),
-                ),
-              ],
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is Authenticated) context.go('/home');
+        if (state is AuthFailure) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text(state.message)));
+        }
+      },
+      builder: (context, state) {
+        if (state is AuthLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        return AppPage(
+          padding: const EdgeInsets.all(24),
+          customCrossAxis: CrossAxisAlignment.center,
+          widgets: [
+            TextField(controller: _email),
+            TextField(controller: _pw, obscureText: true),
+            ElevatedButton(
+              onPressed: () => context
+                  .read<AuthCubit>()
+                  .login(_email.text.trim(), _pw.text.trim()),
+              child: const Text('Sign In'),
             ),
-          );
-        },
-      ),
+            TextButton(
+              onPressed: () => context.go('/sign-up'),
+              child: const Text('Create account'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
+
