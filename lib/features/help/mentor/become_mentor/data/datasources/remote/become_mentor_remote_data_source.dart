524966be04d57d771e0f43fed3811ce388b961db
Jady McIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2025-10-18 14:57:58 +0200

    gpt's changes

--- lib/features/help/mentor/become_mentor/data/datasources/remote/become_mentor_remote_data_source.dart
+++ lib/features/help/mentor/become_mentor/data/datasources/remote/become_mentor_remote_data_source.dart
@@ -1,3 +1,11 @@
-abstract class BecomeMentorRemoteDataSource {
+import '../../models/mentor_application_model.dart';
 
+abstract class BecomeMentorRemoteDataSource {
+  Future<void> submitApplication({
+    required MentorApplicationModel application,
+    required String userId,
+    MentorAttachmentModel? attachment,
+  });
+
+  Future<DateTime?> lastSubmissionAt(String userId);
 }
--- lib/features/help/mentor/become_mentor/domain/repo/become_mentor_repo.dart
+++ lib/features/help/mentor/become_mentor/domain/repo/become_mentor_repo.dart
@@ -1,3 +1,9 @@
-abstract class BecomeMentorRepo {
+import '../entities/mentor_application.dart';
+import '../entities/mentor_attachment.dart';
 
+abstract class BecomeMentorRepo {
+  Future<void> submitApplication(
+    MentorApplication application, {
+    MentorApplicationAttachment? attachment,
+  });
 }
