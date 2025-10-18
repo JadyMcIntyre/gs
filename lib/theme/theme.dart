a4750f29ddcea98bed9a0779e0c873074825c8e4
Jady McIntyre <116111920+JadyMcIntyre@users.noreply.github.com>
2025-10-18 12:01:41 +0200

    fixed input theme stuff

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -34,7 +34,11 @@ ThemeData lightTheme() {
         textStyle: WidgetStateProperty.all(const TextStyle(fontWeight: FontWeight.bold)),
       ),
     ),
-    inputDecorationTheme: InputDecorationTheme(filled: true, fillColor: Colors.white),
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: lightColorScheme.surfaceContainer,
+      focusColor: lightColorScheme.surface,
+    ),
     useMaterial3: true,
   );
 }
@@ -71,7 +75,11 @@ ThemeData darkTheme() {
         textStyle: WidgetStateProperty.all(const TextStyle(fontWeight: FontWeight.bold)),
       ),
     ),
-    inputDecorationTheme: InputDecorationTheme(filled: true, fillColor: Colors.white),
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: darkColorScheme.surfaceContainer,
+      focusColor: darkColorScheme.surface,
+    ),
     useMaterial3: true,
   );
 }
